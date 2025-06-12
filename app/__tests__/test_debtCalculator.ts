e6573eb12af0a5c9cbcead8043a1442732700ad0
SalvadorCSJ <130864614+SalvadorCSJ@users.noreply.github.com>
2025-06-12 10:36:32 -0300

    Added test with 10 people.

--- app/__tests__/test_debtCalculator.ts
+++ app/__tests__/test_debtCalculator.ts
@@ -96,4 +96,31 @@ describe('calculateSettlements', () => {
     { from: 'Bob', to: 'David', amount: 15},
   ]);
 });
+
+  it('calculates simple settlement between 10 people', () => {
+  const people: Person[] = [
+    { id: '1', name: 'Alice', items: [{ id: 'a1', name: 'Bife', value: 150 }] },
+    { id: '2', name: 'Bob', items: [{id: 'b1', name: 'Hambúrguer', value: 50}] },
+    { id: '3', name: 'Carol', items: [{id: 'c1', name: 'Refrigerante', value: 10}]},
+    { id: '4', name: 'David', items: [{id: 'd1', name: 'Macarrão', value: 100}]},
+    { id: '5', name: 'Eve', items:  [{id: 'e1', name: 'Sanduíche', value: 15}]},
+    { id: '6', name: 'Frank', items:  [{id: 'f1', name: 'Salada', value: 30}]},
+    { id: '7', name: 'Grace', items:  [{id: 'g1', name: 'Espetinho', value: 20}]},
+    { id: '8', name: 'Hank', items:  [{id: 'h1', name: 'Sushi', value: 120}]},
+    { id: '9', name: 'Ivy', items:  [{id: 'i1', name: 'Sopa', value: 50}]},
+    { id: '10', name: 'Jack', items:  [{id: 'j1', name: 'Tamagoyaki', value: 45}]},
+  ];
+
+  expect(calculateSettlements(people)).toEqual([
+    { from: 'Carol', to: 'Alice', amount: 49 },
+    { from: 'Eve', to: 'Alice', amount: 42 },
+    { from: 'Eve', to: 'Hank', amount: 2},
+    { from: 'Grace', to: 'Hank', amount: 39},
+    { from: 'Frank', to: 'Hank', amount: 20},
+    { from: 'Frank', to: 'David', amount: 9},
+    { from: 'Jack', to: 'David', amount: 14},
+    { from: 'Bob', to: 'David', amount: 9},
+    { from: 'Ivy', to: 'David', amount: 9},
+  ]);
+});
 });
